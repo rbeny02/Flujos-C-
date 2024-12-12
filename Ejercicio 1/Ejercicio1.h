f589de537c8795853ba5ea4db2ca47761bb6851c
rbeny02 <rbeny@myuax.com>
2024-12-12 12:22:04 +0100

    organización

---
+++ Ejercicio 1/Ejercicio1.h
@@ -0,0 +1,16 @@
+//
+// Created by rania on 12/12/2024.
+//
+
+#ifndef EJERCICIO1_H
+#define EJERCICIO1_H
+
+
+
+class Ejercicio1 {
+
+};
+
+
+
+#endif //EJERCICIO1_H
---
+++ Ejercicio 5/Ejercicio5.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rania on 12/12/2024.
+//
+
+#include "Ejercicio5.h"

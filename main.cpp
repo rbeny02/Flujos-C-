0419d8fcf8de52a154b4d978c6a4dab8bf59a991
rbeny02 <rbeny@myuax.com>
2024-12-16 22:34:37 +0100

    parte 2

--- main.cpp
+++ main.cpp
@@ -3,11 +3,19 @@
 //
 
 #include <iostream>
-
+#include <string>
 int main() {
     int num;
-    std::cout << "Ingresa un número: ";
+    std::cout << "Ingresa un numero: ";
     std::cin >> num;
     std::cout << "El número ingresado es: " << num << std::endl;
+
+
+    std::string text;
+    std::cout << "Ingresa un numero: ";
+    std::cin >> num;
+    std::cout << "Ingresa un texto: ";
+    std::cin >> text;
+    std::cout << "El numero ingresado es: " << num << "\nEl texto ingresado es: " << text << std::endl;
     return 0;
 }
